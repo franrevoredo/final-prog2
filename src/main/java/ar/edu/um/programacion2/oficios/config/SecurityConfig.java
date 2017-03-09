50a552ecbeb6a26345c67964d2747e0389594c2d
Francisco Revoredo <frevoredo@silice.biz>
2017-03-09 04:40:27 -0300

    Terminado favoritos

--- src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
+++ src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
@@ -32,7 +32,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/administradors/**", "/admpanel/**", "/categorias/**", "/disponibilidads/**", "/localidads/**", "/personae/**", "/clientes/**", "/prestadors/list", "/servicios/**").hasAuthority("ROLE_ADMINISTRADOR");
 		http.authorizeRequests().antMatchers("/servicios/create-form", "/calificacionprestadors/**").hasAuthority("ROLE_PRESTADOR");
-		http.authorizeRequests().antMatchers("/calificacionclientes/**").hasAuthority("ROLE_CLIENTE");
+		http.authorizeRequests().antMatchers("/calificacionclientes/**", "/ver-servicio/**", "/add-fav/**", "/ver-cliente/**", "/del-fav/**").hasAuthority("ROLE_CLIENTE");
 		http.authorizeRequests().antMatchers("/buscar/**", "/myprofile").authenticated().and().formLogin().loginPage("/login").and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		http.authorizeRequests().antMatchers("/public/**", "/js/**", "/webjars/**", "/", "/clientes/create-form", "/prestadors/create-form").permitAll().anyRequest().anonymous();
 		http.csrf().disable();
--- src/main/java/ar/edu/um/programacion2/oficios/web/MainController.java
+++ src/main/java/ar/edu/um/programacion2/oficios/web/MainController.java
@@ -1,4 +1,5 @@
 package ar.edu.um.programacion2.oficios.web;
+
 import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,7 +11,9 @@ import org.springframework.roo.addon.web.mvc.thymeleaf.annotations.RooThymeleafM
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.um.programacion2.oficios.domain.Cliente;
@@ -19,6 +22,7 @@ import ar.edu.um.programacion2.oficios.reference.Categoria;
 import ar.edu.um.programacion2.oficios.reference.Localidad;
 import ar.edu.um.programacion2.oficios.reference.Persona;
 import ar.edu.um.programacion2.oficios.service.impl.CategoriaServiceImpl;
+import ar.edu.um.programacion2.oficios.service.impl.ClienteServiceImpl;
 import ar.edu.um.programacion2.oficios.service.impl.LocalidadServiceImpl;
 import ar.edu.um.programacion2.oficios.service.impl.PersonaServiceImpl;
 import ar.edu.um.programacion2.oficios.service.impl.ServicioServiceImpl;
@@ -31,43 +35,99 @@ import ar.edu.um.programacion2.oficios.service.impl.ServicioServiceImpl;
  */
 @RooThymeleafMainController
 public class MainController {
-	
+
 	@Autowired
 	LocalidadServiceImpl localidadService;
-	
+
 	@Autowired
 	ServicioServiceImpl servicioService;
-	
+
 	@Autowired
 	CategoriaServiceImpl catService;
-	
+
 	@Autowired
 	PersonaServiceImpl personaService;
+
+	@Autowired
+	ClienteServiceImpl clienteService;
+
+	@GetMapping("/testuser")
+	public String testUser(Pageable pageable) {
+
+		Cliente c1 = new Cliente("Juan", "Perez", "Niggas 123", localidadService.findOne((long) 2));
+
+		Set<Servicio> favoritos = new HashSet<Servicio>(servicioService.findAll());
+
+		c1.setFavoritos(favoritos);
+
+		System.out.println(servicioService
+				.findByCategoria(catService.findByNombre("Fontanería", pageable).getContent().get(0), pageable)
+				.getContent());
+
+		// System.out.println(c1.toString());
+
+		return "index";
+	}
+
+	@GetMapping("/myprofile")
+	public ModelAndView myProfile(Model model, Principal principal, Pageable pageable) {
+		Persona user = personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		return new ModelAndView("redirect:/ver-cliente/" + user.getId());
+	}
+
+	@GetMapping("/add-fav/{id}")
+	public ModelAndView addFavorite(Model model, Principal principal, Pageable pageable,
+			@PathVariable(value = "id") long id) {
+		Cliente user = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		Servicio servicio = servicioService.findOne(id);
+		Set<Servicio> favoritos = user.getFavoritos();
+		favoritos.add(servicio);
+
+		user.setFavoritos(favoritos);
+
+		clienteService.save(user);
+
+		return new ModelAndView("redirect:/ver-cliente/" + user.getId());
+	}
 	
-    @GetMapping("/testuser")
-    public String testUser(Pageable pageable) {
-    	
-    	Cliente c1 = new Cliente("Juan", "Perez", "Niggas 123", localidadService.findOne((long) 2));
-    	
-    	Set<Servicio> favoritos = new HashSet<Servicio>(servicioService.findAll());
-    	
-    	c1.setFavoritos(favoritos);
-    	
-    	System.out.println(servicioService.findByCategoria(catService.findByNombre("Fontanería", pageable).getContent().get(0), pageable).getContent());
-    	
-    	//System.out.println(c1.toString());
-    	
-        return "index";
-    }
-    
-    @GetMapping("/myprofile")
-    public ModelAndView myProfile(Model model, Principal principal, Pageable pageable) {
-    	Persona user = personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
-    	 return new ModelAndView("redirect:/clientes/" + user.getId());
-    }
-    
-    @GetMapping("/register")
-    public String register(Model model) {
-    	 return "register";
-    }
+	@GetMapping("/del-fav/{id}")
+	public ModelAndView delFavorite(Model model, Principal principal, Pageable pageable,
+			@PathVariable(value = "id") long id) {
+		Cliente user = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		Servicio servicio = servicioService.findOne(id);
+		Set<Servicio> favoritos = user.getFavoritos();
+		favoritos.remove(servicio);
+
+		user.setFavoritos(favoritos);
+
+		clienteService.save(user);
+
+		return new ModelAndView("redirect:/ver-cliente/" + user.getId());
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+		return "register";
+	}
+
+	@GetMapping("/ver-servicio/{id}")
+	public ModelAndView verServicio(@PathVariable(value = "id") long id, Model model) {
+		Servicio servicio = servicioService.findOne(id);
+		model.addAttribute("servicio", servicio);
+		model.addAttribute("mapstring", servicio.getLocalidad().getMapString());
+		return new ModelAndView("servicios/show");
+	}
+
+	@GetMapping("/ver-cliente/{id}")
+	public ModelAndView verCliente(@PathVariable(value = "id") long id, Model model, Principal principal,
+			Pageable pageable) {
+		Cliente cliente = clienteService.findOne(id);
+		Cliente current = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		if (cliente.equals(current)) {
+			Set<Servicio> favoritos = cliente.getFavoritos();
+			model.addAttribute("favoritos", favoritos);
+		}
+		model.addAttribute("cliente", cliente);
+		return new ModelAndView("clientes/show");
+	}
 }
