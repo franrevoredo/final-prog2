f2b0e0ab1c3041c3ade7e35c80a384ef9ca359b0
Francisco Revoredo <frevoredo@silice.biz>
2017-03-09 05:08:35 -0300

    Agregado Buscador por favoritos y por categoría

--- src/main/java/ar/edu/um/programacion2/oficios/web/SearchByController.java
+++ src/main/java/ar/edu/um/programacion2/oficios/web/SearchByController.java
@@ -1,6 +1,8 @@
 package ar.edu.um.programacion2.oficios.web;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ar.edu.um.programacion2.oficios.domain.Cliente;
 import ar.edu.um.programacion2.oficios.domain.Servicio;
 import ar.edu.um.programacion2.oficios.service.impl.CategoriaServiceImpl;
+import ar.edu.um.programacion2.oficios.service.impl.ClienteServiceImpl;
 import ar.edu.um.programacion2.oficios.service.impl.LocalidadServiceImpl;
+import ar.edu.um.programacion2.oficios.service.impl.PersonaServiceImpl;
 import ar.edu.um.programacion2.oficios.service.impl.ServicioServiceImpl;
 
 @Controller
@@ -24,23 +29,35 @@ public class SearchByController {
 
 	@Autowired
 	CategoriaServiceImpl catService;
+	
+	@Autowired
+	PersonaServiceImpl personaService;
+
+	@Autowired
+	ClienteServiceImpl clienteService;
+
 
 	@GetMapping("/buscar-cat")
 	public String buscarCat(Model model) {
 		model.addAttribute("categorias", catService.findAll());
 		return "search/bycategoria";
 	}
+	
+	@GetMapping("/buscar-fav")
+	public String buscarFav(Model model, Principal principal, Pageable pageable) {
+		Cliente user = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		Set<Servicio> favoritos = user.getFavoritos();
+		model.addAttribute("searchby", "Favoritos");
+		model.addAttribute("serviciolist", favoritos);
+		return "search-result";
+	}
 
 	@GetMapping("/buscar-cat/term")
-	public String buscarCat(Model model, @RequestParam("categoria-name") String categoria, Pageable pageable) {
+	public String buscarCat(Model model, @RequestParam("categoria") long idcat, Pageable pageable) {
 		List<Servicio> listaDeServicio = servicioService
-				.findByCategoria(catService.findByNombre(categoria, pageable).getContent().get(0), pageable)
+				.findByCategoria(catService.findOne(idcat), pageable)
 				.getContent();
-		System.out.println("Los Servicios son: ");
-		for (Servicio servicio : listaDeServicio) {
-			System.out.println("\n Nombre: " + servicio.getNombre());
-		}
-		model.addAttribute("searchby", categoria);
+		model.addAttribute("searchby", "Categoría");
 		model.addAttribute("serviciolist", listaDeServicio);
 
 		return "search-result";
