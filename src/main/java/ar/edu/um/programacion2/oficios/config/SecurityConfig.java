0043f0ddb97d1c5d51a76f337435c2d944bae226
Francisco Revoredo <frevoredo@silice.biz>
2017-03-14 00:33:55 -0300

    Mejoradas reglas Spring Security.

--- src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
+++ src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
@@ -30,11 +30,11 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/administradors/**", "/admpanel/**", "/categorias/**", "/disponibilidads/**", "/localidads/**", "/personae/**", "/clientes/list", "/prestadors/list").hasAuthority("ROLE_ADMINISTRADOR");
+		http.authorizeRequests().antMatchers("/administradors/**", "/admpanel/**", "/categorias/create-form", "/disponibilidads/create-form", "/localidads/create-form", "/categorias/list", "/disponibilidads/list", "/localidads/list", "/personae/list", "/clientes/list", "/prestadors/list").hasAuthority("ROLE_ADMINISTRADOR");
 		http.authorizeRequests().antMatchers("/nuevo-servicio", "/calificacionprestadors/**", "/servicios/**").hasAuthority("ROLE_PRESTADOR");
 		http.authorizeRequests().antMatchers("/calificacionclientes/**", "/add-fav/**", "/ver-cliente/**", "/del-fav/**").hasAuthority("ROLE_CLIENTE");
 		http.authorizeRequests().antMatchers("/buscar/**", "/myprofile").authenticated().and().formLogin().loginPage("/login").and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
-		http.authorizeRequests().antMatchers("/public/**", "/js/**", "/webjars/**", "/", "/clientes/create-form", "/prestadors/create-form", "/ver-servicio/**").permitAll().anyRequest().anonymous();
+		http.authorizeRequests().antMatchers("/public/**", "/js/**", "/webjars/**", "/", "/clientes/create-form", "/prestadors/create-form", "/ver-servicio/**", "/categorias/s2", "/disponibilidads/s2", "/localidads/s2").permitAll().anyRequest().anonymous();
 		http.csrf().disable();
 	}
 }
