f9fc04748bc06ab0a19d21dbf45251f4c27ffd93
Francisco Revoredo <frevoredo@silice.biz>
2017-03-14 21:34:55 -0300

    Bug de spring security workaround. AL FIN DIOS

--- src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
+++ src/main/java/ar/edu/um/programacion2/oficios/config/SecurityConfig.java
@@ -9,32 +9,47 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.accept.ContentNegotiationStrategy;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(1)
 class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	 @Autowired
-	 DataSource dataSource;
-	 
-	 @Autowired
-	 public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-	   auth.jdbcAuthentication().dataSource(dataSource)
-	  .usersByUsernameQuery(
-	   "select username, password, enabled from persona where username=?").authoritiesByUsernameQuery(
-			   "select username, dtype from persona where username=?");
-	  
-	 } 
-	
-	
+
+	@Autowired
+	DataSource dataSource;
+
+	@Override
+	public void setContentNegotationStrategy(ContentNegotiationStrategy contentNegotiationStrategy) {
+		super.setContentNegotationStrategy(contentNegotiationStrategy);
+	}
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from persona where username=?")
+				.authoritiesByUsernameQuery("select username, dtype from persona where username=?");
+
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/administradors/**", "/admpanel/**", "/categorias/create-form", "/disponibilidads/create-form", "/localidads/create-form", "/categorias/list", "/disponibilidads/list", "/localidads/list", "/personae/list", "/clientes/list", "/prestadors/list").hasAuthority("ROLE_ADMINISTRADOR");
-		http.authorizeRequests().antMatchers("/nuevo-servicio", "/calificacionprestadors/**", "/servicios/**").hasAuthority("ROLE_PRESTADOR");
-		http.authorizeRequests().antMatchers("/calificacionclientes/**", "/add-fav/**", "/ver-cliente/**", "/del-fav/**").hasAuthority("ROLE_CLIENTE");
-		http.authorizeRequests().antMatchers("/buscar/**", "/myprofile").authenticated().and().formLogin().loginPage("/login").and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
-		http.authorizeRequests().antMatchers("/public/**", "/js/**", "/webjars/**", "/", "/clientes/create-form", "/prestadors/create-form", "/ver-servicio/**", "/categorias/s2", "/disponibilidads/s2", "/localidads/s2").permitAll().anyRequest().anonymous();
+		http.authorizeRequests().antMatchers("/administradors/**", "/admpanel/**", "/categorias/create-form",
+				"/disponibilidads/create-form", "/localidads/create-form", "/categorias/list", "/disponibilidads/list",
+				"/localidads/list", "/personae/list", "/clientes/list", "/prestadors/list")
+				.hasAuthority("ROLE_ADMINISTRADOR");
+		http.authorizeRequests().antMatchers("/nuevo-servicio", "/calificacionprestadors/**", "/servicios/**")
+				.hasAuthority("ROLE_PRESTADOR");
+		http.authorizeRequests()
+				.antMatchers("/calificacionclientes/**", "/add-fav/**", "/ver-cliente/**", "/del-fav/**")
+				.hasAuthority("ROLE_CLIENTE");
+		http.authorizeRequests().antMatchers("/buscar/**", "/myprofile").authenticated().and().formLogin()
+				.loginPage("/login").and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
+		http.authorizeRequests()
+				.antMatchers("/public/**", "/js/**", "/webjars/**", "/", "/clientes/create-form",
+						"/prestadors/create-form", "/ver-servicio/**", "/categorias/s2", "/disponibilidads/s2",
+						"/localidads/s2")
+				.permitAll().anyRequest().anonymous();
 		http.csrf().disable();
 	}
 }
