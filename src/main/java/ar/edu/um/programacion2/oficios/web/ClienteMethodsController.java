3550bec22e5a35b094b5d5b6f41bf6ffd36c352d
Francisco Revoredo <frevoredo@silice.biz>
2017-08-27 21:41:26 -0300

    _Agregado el botón de editar perfil solo al perfil del que está logeado. _Agregado error al método de ver usuario cuando el usuario no existe.

--- src/main/java/ar/edu/um/programacion2/oficios/web/ClienteMethodsController.java
+++ src/main/java/ar/edu/um/programacion2/oficios/web/ClienteMethodsController.java
@@ -18,31 +18,46 @@ import ar.edu.um.programacion2.oficios.service.impl.PersonaServiceImpl;
 
 @Controller
 public class ClienteMethodsController {
-	
+
 	@Autowired
 	PersonaServiceImpl personaService;
 
 	@Autowired
 	ClienteServiceImpl clienteService;
-	
+
 	@GetMapping("/ver-cliente/{id}")
-	public ModelAndView verCliente(@PathVariable(value = "id") long id, Model model, Principal principal,
-			Pageable pageable) {
+	public ModelAndView verCliente(@PathVariable(value = "id") long id,
+			Model model, Principal principal, Pageable pageable) {
+		Boolean authorizedview = false;
 		Cliente cliente = clienteService.findOne(id);
-		Cliente current = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		Cliente current = (Cliente) personaService
+				.findByUsername(principal.getName(), pageable).getContent()
+				.get(0);
+		if (cliente == null) {
+			model.addAttribute("error", "NO EXISTE");
+			model.addAttribute("message",
+					"El cliente al que quiere acceder no existe");
+			model.addAttribute("status", "404");
+			return new ModelAndView("error");
+		}
+
 		if (cliente.equals(current)) {
+			authorizedview = true;
 			Set<Servicio> favoritos = cliente.getFavoritos();
 			model.addAttribute("favoritos", favoritos);
 		}
 		model.addAttribute("cliente", cliente);
+		model.addAttribute("authorizedview", authorizedview);
 		return new ModelAndView("clientes/show");
 	}
-	
+
 	@GetMapping("/editar-cliente/{id}")
-	public ModelAndView editarCliente(@PathVariable(value = "id") long id, Model model, Principal principal,
-			Pageable pageable) {
+	public ModelAndView editarCliente(@PathVariable(value = "id") long id,
+			Model model, Principal principal, Pageable pageable) {
 		Cliente cliente = clienteService.findOne(id);
-		Cliente current = (Cliente) personaService.findByUsername(principal.getName(), pageable).getContent().get(0);
+		Cliente current = (Cliente) personaService
+				.findByUsername(principal.getName(), pageable).getContent()
+				.get(0);
 		if (cliente.equals(current)) {
 			Set<Servicio> favoritos = cliente.getFavoritos();
 			model.addAttribute("favoritos", favoritos);
